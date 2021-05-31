479993581ed467493073eabdcc34293ef045c885
VertexHubYT <80241757+VertexHubYT@users.noreply.github.com>
2021-05-31 17:34:32 +1000

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local rebirth = game:GetService("ReplicatedStorage").Rebirth
 local Load = "Load"
 
 game:GetService("RunService").RenderStepped:Connect(function()
-if Auto_Remote == true then
+if _G.Auto_Remote == true then
         game:GetService("ReplicatedStorage").RemoteDrop:FireServer()
     end
 end)
@@ -11,14 +11,14 @@ local function layouts(player)
 local Load = "Load"
 local check = "Check"
 
-if Use_Second_Layout == false then
+if _G.Use_Second_Layout == false then
     game:GetService("ReplicatedStorage").DestroyAll:InvokeServer()
-    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, Starting_Layout)
-elseif Use_Second_Layout == true then
+    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, _G.Starting_Layout)
+elseif _G.Use_Second_Layout == true then
     game:GetService("ReplicatedStorage").DestroyAll:InvokeServer()
-    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, Starting_Layout)
-    wait(Second_Timer)
-    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, Second_Layout, Items)
+    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, _G.Starting_Layout)
+    wait(_G.Second_Timer)
+    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, _G.Second_Layout, _G.Items)
     end
 end
 
