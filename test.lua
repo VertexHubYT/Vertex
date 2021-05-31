cb2e35b937292a18b2804bb878a563982c95b567
VertexHubYT <80241757+VertexHubYT@users.noreply.github.com>
2021-05-31 17:39:00 +1000

    Update test.lua

--- test.lua
+++ test.lua
@@ -18,7 +18,8 @@ elseif _G.Use_Second_Layout == true then
     game:GetService("ReplicatedStorage").DestroyAll:InvokeServer()
     game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, _G.Starting_Layout)
     wait(_G.Second_Timer)
-    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, _G.Second_Layout, _G.Items)
+    game:GetService("ReplicatedStorage").DestroyAll:InvokeServer()
+    game:GetService("ReplicatedStorage").Layouts:InvokeServer(Load, _G.Second_Layout, _G.Items.costs)
     end
 end
 
